49954f5606be237862e88002747ac8b4ddd27348
mihalevm <0okmax0@gmail.com>
2020-05-10 18:49:37 +0700

    Доработка интефейса

--- js/tcalculator.js
+++ js/tcalculator.js
@@ -1,17 +1,56 @@
-var tcalculator = function(){
-    var __const_voltage_discharge = {'48':43.2, '60':54};
-    var __battery_params_list = null;
+let tcalculator = function() {
+    let __const_voltage_discharge = {'48':43.2, '60':54};
+    let __battery_params_list = null;
+    let __chart_holder = null;
+
+    let __chart_config = {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'ток разряда АКБ',
+                data: [],
+                backgroundColor: 'red',
+                borderColor: 'red',
+                fill: false,
+                borderDash: [5, 5],
+                pointRadius: 5,
+                pointHoverRadius: 10,
+            },
+                {
+                    label: 'расчетный ток',
+                    fill: false,
+                    backgroundColor: 'blue',
+                    borderColor: 'blue',
+                    data: [],
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            legend: {position: 'top',},
+            hover: {mode: 'index'},
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {display: true,labelString: 'Время'}
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {display: true,labelString: 'Ток (А)'}
+                }]
+            },
+            title: {display: true,text: ''}
+        }
+    };
 
     function __timeConvert(n) {
-        var num = n;
-        var hours = (num / 60);
-        var rhours = Math.floor(hours);
-        var minutes = (hours - rhours) * 60;
-        var rminutes = Math.round(minutes);
+        let hours = (n / 60);
+        let rhours = Math.floor(hours);
+        let minutes = (hours - rhours) * 60;
+        let rminutes = Math.round(minutes);
 
-        num = num < 60 ? num+'мин' : (rminutes === 0 ? rhours+'ч' : rhours+'ч '+ rminutes + 'м');
-
-        return num;
+        return n < 60 ? n+'мин' : (rminutes === 0 ? rhours+'ч' : rhours+'ч '+ rminutes + 'м');
     }
 
     function __reset_selection(sel){
@@ -34,6 +73,8 @@ var tcalculator = function(){
             $('#premove').mask('00000');
             $('#pcurrent').mask('00000');
             $('#pproj').mask('00000');
+
+            __chart_holder = new Chart(document.getElementById('canvas').getContext('2d'), __chart_config);
 
             $('#calculate').click(function () {
                 if (tcalculator.validate()){
@@ -82,7 +123,7 @@ var tcalculator = function(){
         },
         validate: function () {
             tcalculator.resetValidation();
-            var is_validate = true;
+            let is_validate = true;
 
             $(['voltage', 'vendor', 'btype', 'bcapacity', 'bgroups']).each(function (i, o) {
                 if (isNaN(parseInt($('#'+o+' option:selected').val()))){
@@ -101,8 +142,8 @@ var tcalculator = function(){
             return is_validate;
         },
         calculateTime:function () {
-            var p_sum       = parseFloat($('#pcurrent').val()) - parseFloat($('#premove').val()) + parseFloat($('#pproj').val());
-            var i_discharge = p_sum/__const_voltage_discharge[$('#voltage option:selected').val()];
+            let p_sum       = parseFloat($('#pcurrent').val()) - parseFloat($('#premove').val()) + parseFloat($('#pproj').val());
+            let i_discharge = p_sum/__const_voltage_discharge[$('#voltage option:selected').val()];
 
             console.log('Ток разряда Ip:'+i_discharge);
 
@@ -113,41 +154,41 @@ var tcalculator = function(){
 
         },
         showChart: function (id) {
-            var pid = $('#bcapacity option:selected').val();
+            let pid = $('#bcapacity option:selected').val();
 
             if (!isNaN(pid) && parseInt(pid) > 0 ){
                 $(__battery_params_list).each(function (i, o) {
                     if (o.pid === pid) {
-                        config.options.title.text='График разряда АКБ ('+o.name+')';
-                        config.data.labels = [];
-                        config.data.datasets[0].data = [];
-                        config.data.datasets[1].data = [];
-                        for (v in o.discharge){
-                            config.data.labels.push(__timeConvert(parseInt(v)));
-                            config.data.datasets[0].data.push(parseFloat(o.discharge[v]));
-                            config.data.datasets[1].data.push(parseFloat(id));
+                        __chart_config.options.title.text='График разряда АКБ ('+o.name+')';
+                        __chart_config.data.labels = [];
+                        __chart_config.data.datasets[0].data = [];
+                        __chart_config.data.datasets[1].data = [];
+                        for (let v in o.discharge){
+                            __chart_config.data.labels.push(__timeConvert(parseInt(v)));
+                            __chart_config.data.datasets[0].data.push(parseFloat(o.discharge[v]));
+                            __chart_config.data.datasets[1].data.push(parseFloat(id));
                         }
                     }
                 })
             }
 
-            myLine.update(config);
+            __chart_holder.update(__chart_config);
             $('.chart-wrapper').show();
 
         },
         getTimeByAmperage: function (id) {
-            var pid = $('#bcapacity option:selected').val();
-            var tres = 0;
+            let pid = $('#bcapacity option:selected').val();
+            let tres = 0;
 
             if (!isNaN(pid) && parseInt(pid) > 0 ){
                 $(__battery_params_list).each(function (i, o) {
                     if (o.pid === pid) {
-                        var x = [];
-                        var y = [];
-                        var pi = 99999999;
-                        var pt = 0;
+                        let x = [];
+                        let y = [];
+                        let pi = 99999999;
+                        let pt = 0;
 
-                        for (v in o.discharge){
+                        for (let v in o.discharge){
                             if (id <= pi && id >= parseFloat(o.discharge[v])) {
                                 x.push(pt);
                                 y.push(pi);
@@ -173,7 +214,7 @@ var tcalculator = function(){
             }
         },
         onVendorSelected: function() {
-            var vid = $('#vendor option:selected').val();
+            let vid = $('#vendor option:selected').val();
 
             if (!isNaN(vid) && parseInt(vid)>0){
                 tcalculator.loadBatteryTypes(vid);
@@ -182,8 +223,8 @@ var tcalculator = function(){
             }
         },
         onBattetyTypeSelected: function() {
-            var vid = $('#vendor option:selected').val();
-            var tid = $('#btype option:selected').val();
+            let vid = $('#vendor option:selected').val();
+            let tid = $('#btype option:selected').val();
 
             if (!isNaN(vid) && !isNaN(tid) && parseInt(vid)>0 && parseInt(tid)>0){
                 tcalculator.loadBatteryParams(vid, tid);
@@ -276,60 +317,4 @@ var tcalculator = function(){
 
 $(document).ready(function(){
     tcalculator.init();
-
-    window.myLine = new Chart(document.getElementById('canvas').getContext('2d'), config);
 });
-
-var config = {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'ток разряда АКБ',
-            data: [],
-            backgroundColor: 'red',
-            borderColor: 'red',
-            fill: false,
-            borderDash: [5, 5],
-            pointRadius: 5,
-            pointHoverRadius: 10,
-        },
-            {
-                label: 'расчетный ток',
-                fill: false,
-                backgroundColor: 'blue',
-                borderColor: 'blue',
-                data: [],
-            }
-        ]
-    },
-    options: {
-        responsive: true,
-        legend: {
-            position: 'top',
-        },
-        hover: {
-            mode: 'index'
-        },
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Время'
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Ток (А)'
-                }
-            }]
-        },
-        title: {
-            display: true,
-            text: ''
-        }
-    }
-};
